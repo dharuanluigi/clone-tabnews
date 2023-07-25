4f914507254c4c6a663a1642496df21b48cc8c4d
Dharuan Luigi <dharuanluigi@gmail.com>
2023-07-24 22:52:36 -0300

    Added: fist page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+function Home() {
+  return (
+    <h1>
+      O copo não se importa em como você o vê, importante é como você o enche. -
+      metoo
+    </h1>
+  );
+}
+
+export default Home;
